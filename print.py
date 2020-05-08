0ef05053e679a13f5e1b6fa19876d584fcbb09a1
mo <mkarimidevops@gmail.com>
2020-05-08 01:41:14 -0500

    print added

---
+++ print.py
@@ -0,0 +1,4 @@
+line_one = "Lines that start with hashtags are comments"
+print(line_one.capitalize())
+line_two = "Write your code below that prints out, 'hello world'"
+print(line_two.capitalize())
