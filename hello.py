de8ed51d93adbf6b9ecd321383d0ee8384991d64
mo <mkarimidevops@gmail.com>
2020-05-08 01:12:28 -0500

    first python code

---
+++ hello.py
@@ -0,0 +1,11 @@
+import datetime
+name= (input("enter your name: "))
+age = (input("enter your current age: "))
+now = datetime.datetime.now()
+
+year = str((int(now.year) - int(age))+100)
+print(name + ", you will be 100 years old in the year " + year)
+
+
+
+
