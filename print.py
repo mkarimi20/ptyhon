2994ee46acfd3f882e6a266d9649869fe300065b
mo <mkarimidevops@gmail.com>
2020-05-08 01:53:50 -0500

    indexing

--- print.py
+++ print.py
@@ -2,3 +2,9 @@ line_one = "Lines that start with hashtags are comments"
 print(line_one.capitalize())
 line_two = "Write your code below that prints out, 'hello world'"
 print(line_two.capitalize())
+
+line_one[3]
+print(line_one[3])
+print(line_one.__len__())
+print(line_one[5:8])
+print(line_one[::-1])
