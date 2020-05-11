39074080618900e7da819cf1245ebef289a16654
Mohammad Karimi <mohammadkarimi@MacBook-Pro.local>
2020-05-11 15:08:49 -0500

    fixed spacing

--- max_finder.py
+++ max_finder.py
@@ -1,14 +1,10 @@
 # Find the maximum of the list of given number
 
-
 def max_finder2(a,b):
-
     if  a > b:
         return a
     return b
     
-
 def max_finder3(a,b,c):
-    
     return max_finder2(a, max_finder2(b,c))
 print(max_finder3(3,7,-7))
