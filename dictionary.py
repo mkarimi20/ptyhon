371eae8754e24a5eda4e00168bc38af6891e737c
Mohammad Karimi <mohammadkarimi@MacBook-Pro.local>
2020-05-11 11:57:01 -0500

    set and dictinary added

---
+++ dictionary.py
@@ -0,0 +1,5 @@
+my_dic = {'key1':12,'key2':13,'key3':14,'key4':'test'}
+print(my_dic)
+
+print(my_dic['key1'])
+print(my_dic['key4'][2])
---
+++ set.py
@@ -0,0 +1,2 @@
+my_list = [1,2,3,4,3,5,8,'work',0]
+print(set(my_list))
