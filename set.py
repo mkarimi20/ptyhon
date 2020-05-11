07a6a8b972a8d054a2844abf2d52cc3adc90bad6
Mohammad Karimi <mohammadkarimi@MacBook-Pro.local>
2020-05-11 13:46:37 -0500

    tax calculator

--- set.py
+++ set.py
@@ -1,2 +1,3 @@
 my_list = [1,2,3,4,3,5,8,'work',0]
 print(set(my_list))
+x= ('some text')
--- tax.py
+++ tax.py
@@ -1,17 +1,17 @@
 usd_to_afs = 80
 currnet_income = 1500
 income_in_afs = usd_to_afs*currnet_income
-print(income_in_afs)
-def tax(income_in_afs):
+def tax_calculator(income_in_afs):
     if income_in_afs >= 100000:
-        tax_in_afs = int(((income_in_afs) - 100000) * (20/100)) + 8900
+        tax_in_afs = (((income_in_afs) - 100000) * (20/100)) + 8900
+        print("your tax in Afghani will be " + str(tax_in_afs))
+        tax_in_usd = tax_in_afs/usd_to_afs
+        print("your tax in USD will be " + str(tax_in_usd))
+    elif income_in_afs >= 12500:
+        tax_in_afs = (((income_in_afs) - 100000) * (10/100))
         print("your tax in Afghani will be" + str(tax_in_afs))
         tax_in_usd = tax_in_afs/usd_to_afs
         print("your tax in USD will be" + str(tax_in_usd))
-    elif income_in_afs >= 12000:
-        tax_in_afs = int(((income_in_afs) - 100000) * (10/100))
-        print("your tax in Afghani will be" + str(tax_in_afs))
-        tax_in_usd = tax_in_afs/usd_to_afs
-        print("your tax in USD will be" + str(tax_in_usd))
-
-    print(tax(income_in_afs)) 
+    else:
+        pass
+print(tax_calculator(income_in_afs))
