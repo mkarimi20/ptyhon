b8ce1c38df558b8398ec7b4eb829d17865738b9e
Mohammad Karimi <mohammadkarimi@MacBook-Pro.local>
2020-05-17 22:41:40 -0500

    added extra

---
+++ forloop.py
@@ -0,0 +1,17 @@
+mylist = [1,2,3,4,5,6,7,8,9,10]
+
+def for_loop(mylist):
+    for test_num in mylist:
+        #check for even number
+        if test_num % 2 == 0:
+            print(test_num)
+for_loop(mylist)
+
+
+
+second_list = 0
+
+for test_num in second_list:
+    second_list = second_list + test_num
+
+print(second_list)
