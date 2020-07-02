43929df84f811e6bf680ccf9eb62e5b3fd0df0f7
Mohammad Karimi <mohammadkarimi@MacBook-Pro.local>
2020-07-02 01:09:33 -0500

    class for atm updated

--- atm_class.py
+++ atm_class.py
@@ -1,30 +1,29 @@
 class ATM:
     print("\nPlease select your choice from the following options: \n\t 1) Balance \n\t 2) withdraw \n\t 3) Deposit \n\t 4) Quit")
-    input_action = int(input("Enter your selected number: "))
+    #input_action = int(input("Enter your selected number: "))
     currnet_balance = 850
 
     def __init__(self, cash):
         self.cash = cash
 
     def balance(self):
-        if input_action == 1:
-            return currnet_balance
+        return self.currnet_balance
 
     def withdraw(self):
-        if input_action == 2:
-            withdraw_amount = float(input('How much do you want to withdraw: '))
-            print(currnet_balance-withdraw_amount)
-    
+        withdraw_amount = float(input('How much do you want to withdraw: '))
+        print(self.currnet_balance-withdraw_amount)
+
     def deposit(self):
-        if input_action == 2:
-            deposit_amount = float(input('How much do you want to add: '))
-            print(currnet_balance+deposit_amount)
+        deposit_amount = float(input('How much do you want to add: '))
+        print(self.currnet_balance+deposit_amount)
     
     def pay_bill(self):
-        if input_action == 2:
-            pay_amount = float(input('How much do you want to pay: '))
-            print(currnet_balance-pay_amount)
+        pay_amount = float(input('How much do you want to pay: '))
+        print(self.currnet_balance-pay_amount)
 
 
 user = ATM('Karimi')
 print(user.balance())
+print(user.withdraw())
+print(user.deposit())
+print(user.pay_bill())
